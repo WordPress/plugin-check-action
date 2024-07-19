6f7d32b40cff142fabcd18612780a0afc9e8d6ed
Pascal Birchler <pascalb@google.com>
2024-07-19 13:11:02 +0200

    Add test plugin

---
+++ tests/fixtures/plugin-with-dependencies/plugin-with-dependencies.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Plugin With Dependencies
+Plugin URI: http://example.com
+Description: This is just a plugin.
+Version: 1.0.0
+Requires Plugins: hello-dolly, performance-lab
+*/
