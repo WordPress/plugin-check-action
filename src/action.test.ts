a9264ea6238b86600d2c00571b8b2c509ad5bb91
Pascal Birchler <pascalb@google.com>
2026-08-16 21:02:01 +0200

    Try to reproduce WooCommerce debug output

--- src/action.test.ts
+++ src/action.test.ts
@@ -15,4 +15,17 @@ describe('action.yml', () => {
 			/command:\s*\|\s*wp-env start --update\s+wp-env run cli wp cli info/,
 		);
 	});
+
+	it('dumps debug state when plugin activation fails', () => {
+		expect(actionYml).toContain('dump_debug_state()');
+
+		// Both the dependency install and the plugin activation need to report
+		// the environment state before bailing.
+		expect(actionYml).toMatch(
+			/if ! wp-env run cli wp plugin install --activate \$DEPENDENCIES; then[\s\S]*?dump_debug_state/,
+		);
+		expect(actionYml).toMatch(
+			/if ! wp-env run cli wp plugin activate \$PLUGIN_SLUG; then[\s\S]*?dump_debug_state/,
+		);
+	});
 });
---
+++ tests/fixtures/plugin-with-woocommerce-dependency/plugin-with-woocommerce-dependency.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Plugin With WooCommerce Dependency
+Plugin URI: http://example.com
+Description: This is just a plugin. It exists to reproduce https://github.com/WordPress/plugin-check-action/issues/611.
+Version: 1.0.0
+Requires Plugins: woocommerce
+*/
